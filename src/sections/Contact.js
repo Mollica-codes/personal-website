da2b38f7159ef65174ad4789cb313cb34955b5b6
Chris Mollica <cjohnm8@gmail.com>
2022-03-08 14:34:48 -0500

    update linkedIn link
    updated the linkedin link in contact.js to reflect new change in domain

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
             <div className="contact-links">
                 <a href="https://github.com/Mollica-codes" target="_blank" className="contact-icons" rel="noreferrer"><i class="fa-brands fa-github"></i></a>
                 <a href="https://codepen.io/MollicaCodes" target="_blank" className="contact-icons" rel="noreferrer"><i class="fa-brands fa-codepen"></i></a>
-                <a href="https://www.linkedin.com/in/chris-mollica-502282173/" target="_blank" className="contact-icons" rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://www.linkedin.com/in/christophermollica/" target="_blank" className="contact-icons" rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a>
                 <a href="https://www.freecodecamp.org/mollicacodes" target="_blank" className="contact-icons" rel="noreferrer"><i class="fa-brands fa-free-code-camp"></i></a>
             </div>
             <p>Email: Mollica.26@osu.edu</p>
